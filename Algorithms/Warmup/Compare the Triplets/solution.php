22781f7a2fa026f1f2daf59133d284e7897b1afa
Anarya Indika Putra <57693674+anaryaputra@users.noreply.github.com>
2021-09-07 19:16:02 +0700

    Create solution.php

---
+++ Algorithms/Warmup/Compare the Triplets/solution.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * Complete the 'compareTriplets' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+function compareTriplets($a, $b) {
+    // Write your code here
+    $points = [0,0];
+    for ($i=0; $i<count($a); $i++) {
+        if ($a[$i] > $b[$i]) {
+            $points[0]++;
+        } else if ($a[$i] < $b[$i]) {
+            $points[1]++;
+        }
+    }
+    return $points;
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$a_temp = rtrim(fgets(STDIN));
+
+$a = array_map('intval', preg_split('/ /', $a_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+$b_temp = rtrim(fgets(STDIN));
+
+$b = array_map('intval', preg_split('/ /', $b_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+$result = compareTriplets($a, $b);
+
+fwrite($fptr, implode(" ", $result) . "\n");
+
+fclose($fptr);
