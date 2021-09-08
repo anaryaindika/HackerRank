1d4f8d4a76927f6c7f143f20e479000a67f7cfbb
Anarya Indika Putra <57693674+anaryaputra@users.noreply.github.com>
2021-09-08 12:37:43 +0700

    Create solution.js

---
+++ Algorithms/Warmup/Birthday Cake Candles/solution.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'birthdayCakeCandles' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY candles as parameter.
+ */
+
+function birthdayCakeCandles(candles) {
+    // Write your code here
+    const tallestCandle = Math.max(...candles);    
+    const tallestCandleAmount = candles.reduce((counter,candle) => candle == tallestCandle ? counter += 1 : counter, 0);
+    return tallestCandleAmount;    
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const candlesCount = parseInt(readLine().trim(), 10);
+
+    const candles = readLine().replace(/\s+$/g, '').split(' ').map(candlesTemp => parseInt(candlesTemp, 10));
+
+    const result = birthdayCakeCandles(candles);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
