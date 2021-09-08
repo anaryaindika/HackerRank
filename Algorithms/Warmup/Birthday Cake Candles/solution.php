aa6e936e5e5414be958d4a34d622e974a95194a8
Anarya Indika Putra <57693674+anaryaputra@users.noreply.github.com>
2021-09-08 11:50:54 +0700

    Create solution.php

---
+++ Algorithms/Warmup/Birthday Cake Candles/solution.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * Complete the 'birthdayCakeCandles' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY candles as parameter.
+ */
+
+function birthdayCakeCandles($candles) {
+    // Write your code here
+    return array_count_values($candles)[max($candles)];
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$candles_count = intval(trim(fgets(STDIN)));
+
+$candles_temp = rtrim(fgets(STDIN));
+
+$candles = array_map('intval', preg_split('/ /', $candles_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+$result = birthdayCakeCandles($candles);
+
+fwrite($fptr, $result . "\n");
+
+fclose($fptr);
