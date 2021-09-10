69a57870e15e955903c234c1a253f967ae15b0f5
Anarya Indika Putra <57693674+anaryaputra@users.noreply.github.com>
2021-09-10 12:51:42 +0700

    Create solution.js

---
+++ Algorithms/Implementation/Migratory Birds/solution.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'migratoryBirds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function migratoryBirds(arr) {
+    // Write your code here
+    var maxFrequency = 0, lowestMostFrequentType = arr[0];        
+    for (let i=1; i<=5; i++) {
+        let frequency = arr.reduce((counter,type) => counter+(type == i),0);
+        if (frequency > maxFrequency) {
+            maxFrequency = frequency;
+            lowestMostFrequentType = i;
+        }
+    }    
+    return lowestMostFrequentType;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arrCount = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = migratoryBirds(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
