452d925a3477483dede705ae6955bb12c8fdca65
Anarya Indika Putra <57693674+anaryaputra@users.noreply.github.com>
2021-09-10 11:05:04 +0700

    Create solution.java

---
+++ Algorithms/Implementation/Migratory Birds/solution.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'migratoryBirds' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int migratoryBirds(List<Integer> arr) {
+    // Write your code here        
+        Map<Integer,Integer> typeFrequencies = new HashMap<Integer,Integer>();
+        for (int type : arr) {
+            Integer frequency = typeFrequencies.get(type);
+            typeFrequencies.put(type,(frequency == null) ? 1 : frequency+1);
+        }
+        int maxFrequency = 0;
+        int lowestMostFrequentType = arr.get(0);
+        for (Map.Entry<Integer,Integer> type : typeFrequencies.entrySet()) {
+            if (type.getValue() > maxFrequency) {
+                maxFrequency = type.getValue();
+                lowestMostFrequentType = type.getKey();
+            }            
+        }        
+        return lowestMostFrequentType;            
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.migratoryBirds(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
