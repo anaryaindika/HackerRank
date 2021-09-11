4b367fd1f5f1bb67f88848b3a8a55b9dc355c1ef
Anarya Indika Putra <57693674+anaryaputra@users.noreply.github.com>
2021-09-11 14:27:44 +0700

    Create solution.php

---
+++ Algorithms/Implementation/Cats and Mouse/solution.php
@@ -0,0 +1,38 @@
+<?php
+
+// Complete the catAndMouse function below.
+function catAndMouse($x, $y, $z) {    
+    $cat_a_distance = abs($z-$x);
+    $cat_b_distance = abs($z-$y);
+    if ($cat_a_distance > $cat_b_distance) {
+        return "Cat B";
+    } else if ($cat_a_distance < $cat_b_distance) {
+        return "Cat A";
+    } else {
+        return "Mouse C";
+    }
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$stdin = fopen("php://stdin", "r");
+
+fscanf($stdin, "%d\n", $q);
+
+for ($q_itr = 0; $q_itr < $q; $q_itr++) {
+    fscanf($stdin, "%[^\n]", $xyz_temp);
+    $xyz = explode(' ', $xyz_temp);
+
+    $x = intval($xyz[0]);
+
+    $y = intval($xyz[1]);
+
+    $z = intval($xyz[2]);
+
+    $result = catAndMouse($x, $y, $z);
+
+    fwrite($fptr, $result . "\n");
+}
+
+fclose($stdin);
+fclose($fptr);
