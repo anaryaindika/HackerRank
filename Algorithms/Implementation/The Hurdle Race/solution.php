c14d0f7c0226160aa47fd7542bdcf14ec46cca3a
Anarya Indika Putra <57693674+anaryaputra@users.noreply.github.com>
2021-09-18 08:43:22 +0700

    Create solution.php

---
+++ Algorithms/Implementation/The Hurdle Race/solution.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * Complete the 'hurdleRace' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY height
+ */
+
+function hurdleRace($k, $height) {
+    // Write your code here
+    $maxHurdleHeight = max($height);
+    if ($k >= $maxHurdleHeight) {
+        return 0;
+    } else {
+        return $maxHurdleHeight-$k;
+    }
+    
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$first_multiple_input = explode(' ', rtrim(fgets(STDIN)));
+
+$n = intval($first_multiple_input[0]);
+
+$k = intval($first_multiple_input[1]);
+
+$height_temp = rtrim(fgets(STDIN));
+
+$height = array_map('intval', preg_split('/ /', $height_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+$result = hurdleRace($k, $height);
+
+fwrite($fptr, $result . "\n");
+
+fclose($fptr);
