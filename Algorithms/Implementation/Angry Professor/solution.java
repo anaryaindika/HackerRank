77c914d07fb36f9b230f66f2e0d8853e7190128c
Anarya Indika Putra <57693674+anaryaputra@users.noreply.github.com>
2021-09-18 15:33:40 +0700

    Create solution.java

---
+++ Algorithms/Implementation/Angry Professor/solution.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'angryProfessor' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY a
+     */
+
+    public static String angryProfessor(int k, List<Integer> a) {
+        /* Initialize variables.
+         *  1. INTEGER threshold : threshold of mininum attendess.
+         *  2. ARRAYLIST INTEGER arrivalTimes : arrival time of each student.
+         *  3. INTEGER onTimeStudent : amount of on time student.
+        */
+        int threshold = k;
+        List<Integer> arrivalTimes = a;
+        int onTimeStudent = 0;    
+            
+        /* Iterate through each student arrival time and count how many student are on time.
+        * Check if threshold is fulfilled. Return "YES if threshold fulfilled."
+        */
+        for (int i=0; i<arrivalTimes.size(); i++) {
+            if (arrivalTimes.get(i) <= 0) {
+                onTimeStudent++;
+            }
+            if (onTimeStudent == threshold) {
+                return "NO";
+            }
+        }
+        return "YES";
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int n = Integer.parseInt(firstMultipleInput[0]);
+
+                int k = Integer.parseInt(firstMultipleInput[1]);
+
+                List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+                String result = Result.angryProfessor(k, a);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
