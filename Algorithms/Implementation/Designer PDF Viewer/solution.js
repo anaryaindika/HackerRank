b4b1a0aa7ee8d607040f1e41fd1b61e76cea6e90
Anarya Indika Putra <57693674+anaryaputra@users.noreply.github.com>
2021-09-18 09:15:00 +0700

    Create solution.js

---
+++ Algorithms/Implementation/Designer PDF Viewer/solution.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'designerPdfViewer' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY h
+ *  2. STRING word
+ */
+
+function designerPdfViewer(h, word) {
+    // Write your code here    
+    var alphabets = [];
+    var maxHeight = 0;
+    for (let char of "abcdefghijklmnopqrstuvwxyz") {        
+        alphabets.push(char);
+    }
+    for (let char of word) {
+        let index = alphabets.indexOf(char);
+        if (h[index] > maxHeight) {
+            maxHeight = h[index];
+        }
+    }
+    let area = word.length*maxHeight;    
+    return area;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const h = readLine().replace(/\s+$/g, '').split(' ').map(hTemp => parseInt(hTemp, 10));
+
+    const word = readLine();
+
+    const result = designerPdfViewer(h, word);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
